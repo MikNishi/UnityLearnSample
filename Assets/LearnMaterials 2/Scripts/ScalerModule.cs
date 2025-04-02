51e6fdf1a397fc186c6bd0fbe17ffa34ac127477
MikNishi <miknishi1584@gmail.com>
2025-04-02 19:57:37 +0700

    Исправления сцен + скрипты первой части

--- Assets/LearnMaterials 2/Scripts/ScalerModule.cs
+++ Assets/LearnMaterials 2/Scripts/ScalerModule.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1rdTEVSrCcYOjqTJcFCHj46RvnbdJhmQUb3gHMDhVftI/edit?usp=sharing")]
 public class ScalerModule : MonoBehaviour
 {
+    [SerializeField]
     private Vector3 targetScale = new Vector3(2,2,2);
 
+    [SerializeField, Min(0.1f)]
     private float changeSpeed;
 
     private Vector3 defaultScale;
     private Transform myTransform;
+    [SerializeField]
     private bool toDefault;
 
     private void Start()
@@ -19,6 +22,7 @@ public class ScalerModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Начать выполнение")]
     public void ActivateModule()
     {
         Vector3 target = toDefault ? defaultScale : targetScale;
@@ -27,6 +31,7 @@ public class ScalerModule : MonoBehaviour
         toDefault = !toDefault;
     }
 
+    [ContextMenu("Вернуть в дефотное состояние")]
     public void ReturnToDefaultState()
     {
         toDefault = true;
--- Assets/LearnMaterials 2/Scripts/TransparentModule.cs
+++ Assets/LearnMaterials 2/Scripts/TransparentModule.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1Cmm__cbik5J8aHAI6PPaAUmEMF3wAcNo3rpgzsYPzDM/edit?usp=sharing")]
 public class TransparentModule : MonoBehaviour
 {
+    [SerializeField, Min(0.1f)]
     private float changeSpeed;
 
     private float defaultAlpha;
@@ -19,6 +20,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = false;
     }
 
+    [ContextMenu("Activate module")]
     public void ActivateModule()
     {
         float target = toDefault ? defaultAlpha : 0;
@@ -27,6 +29,7 @@ public class TransparentModule : MonoBehaviour
         toDefault = !toDefault;
     }
 
+    [ContextMenu("Возвращение к дефолту")]
     public void ReturnToDefaultState()
     {
         toDefault = true;
